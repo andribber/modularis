32826e6f29feadaac353508eecdc5c670c8f2c8f
Marcos Barbalho <marcos.barbalho.w@gmail.com>
2024-10-14 19:26:52 -0300

    refactor: atualiza enum de Role

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -2,19 +2,39 @@
 
 namespace App\Enums;
 
+use App\Enums\MetaProperties\Role\CanBeAssigned;
+use App\Traits\InteractsWithEnumsMetaProperties;
+use ArchTech\Enums\Meta\Meta;
+use ArchTech\Enums\Metadata;
 use ArchTech\Enums\Values;
 
+/**
+ * @method bool canBeAssigned()
+ */
+#[Meta(CanBeAssigned::class)]
 enum Role: string
 {
+    use InteractsWithEnumsMetaProperties;
+    use Metadata;
     use Values;
 
+    #[CanBeAssigned(true)]
     case ADMIN = 'admin';
 
+    #[CanBeAssigned(false)]
     case OWNER = 'owner';
 
+    #[CanBeAssigned(false)]
     case PERSONAL = 'personal';
 
+    #[CanBeAssigned(true)]
     case OPERATOR = 'operator';
 
+    #[CanBeAssigned(true)]
     case VIEWER = 'viewer';
+
+    public static function assignableRoles(bool $toValue = false): array
+    {
+        return self::getInfoFromMetadata(true, CanBeAssigned::class, $toValue);
+    }
 }
