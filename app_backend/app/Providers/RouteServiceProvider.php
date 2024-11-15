aa63e165f724b186eb0ead3e28d3646faa6cbdf0
Marcos Barbalho <marcos.barbalho.w@gmail.com>
2024-11-15 12:55:15 -0300

    refactor: atualiza RouteServiceProvider

--- app_backend/app/Providers/RouteServiceProvider.php
+++ app_backend/app/Providers/RouteServiceProvider.php
@@ -20,9 +20,8 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('v1')
-                ->group(base_path('routes/v1/api.php'));
+            $this->mapApiRoutes();
+            $this->mapAppRoutes();
         });
     }
 
@@ -37,7 +36,7 @@ class RouteServiceProvider extends ServiceProvider
         foreach (config('app.rate_limit') as $group => $value) {
             RateLimiter::for(
                 $group,
-                fn (Request $request) => Limit::perMinute($value)
+                fn(Request $request) => Limit::perMinute($value)
                     ->by($request->user()?->id ?? $request->ip()),
             );
         }
@@ -46,5 +45,24 @@ class RouteServiceProvider extends ServiceProvider
     protected function getThrottleMiddlewareClass(): string
     {
         return config('cache.default') === 'redis' ? ThrottleRequestsWithRedis::class : ThrottleRequests::class;
+    }
+
+    protected function mapAppRoutes(): void
+    {
+        $domain = sprintf('app.%s', config('app.url'));
+
+        Route::middleware('api')
+            ->domain($domain)
+            ->group(base_path('routes/v1/app.php'));
+    }
+
+    protected function mapApiRoutes(): void
+    {
+        $domain = sprintf('api.%s', config('app.url'));
+
+        Route::middleware('api')
+            ->prefix('v1')
+            ->domain($domain)
+            ->group(base_path('routes/v1/api.php'));
     }
 }
