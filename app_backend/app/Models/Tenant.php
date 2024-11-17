30f61516bc8fb8068695447b969fc9a67f5f136c
Marcos Barbalho <marcos.barbalho.w@gmail.com>
2024-11-17 16:42:52 -0300

    refactor: atualiza models

--- app_backend/app/Models/Tenant.php
+++ app_backend/app/Models/Tenant.php
@@ -2,7 +2,9 @@
 
 namespace App\Models;
 
+use App\Enums\Tenant\Role;
 use App\Traits\InteractsWithObject;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
@@ -38,5 +40,47 @@ class Tenant extends Model
             ->using(TenantUser::class)
             ->withTimestamps()
             ->withPivot('role');
+    }
+
+    protected function responsible(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->users()
+                ->where('role', Role::PERSONAL)
+                ->firstOr(
+                    fn () => $this->users()
+                        ->where('role', Role::OWNER)
+                        ->first(),
+                ),
+        );
+    }
+
+    public function isAdmin(User $user): bool
+    {
+        return $this->users()->where('user_id', $user->id)->where('role', Role::ADMIN)->exists();
+    }
+
+    public function isOwner(User $user): bool
+    {
+        return $this->users()->where('user_id', $user->id)->where('role', Role::OWNER)->exists();
+    }
+
+    public function isPersonal(User $user): bool
+    {
+        return $this->users()->where('user_id', $user->id)->where('role', Role::PERSONAL)->exists();
+    }
+
+    protected function hasOwner(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->users()->where('role', Role::OWNER)->exists(),
+        );
+    }
+
+    protected function hasPersonal(): Attribute
+    {
+        return Attribute::make(
+            get: fn () => $this->users()->where('role', Role::PERSONAL)->exists(),
+        );
     }
 }
