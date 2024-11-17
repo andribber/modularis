ffa514fbb1f65f9d57b419eb93688ba80042643c
Marcos Barbalho <marcos.barbalho.w@gmail.com>
2024-11-17 16:43:52 -0300

    refactor: atualiza controllers de User e Token

--- app_backend/app/Http/Controllers/V1/TokenController.php
+++ app_backend/app/Http/Controllers/V1/TokenController.php
@@ -2,16 +2,20 @@
 
 namespace App\Http\Controllers\V1;
 
-use App\Enums\Role;
+use App\Enums\Tenant\Role;
 use App\Http\Controllers\Controller;
+use App\Http\Queries\TokenQuery;
+use App\Http\Requests\Token\CreateRequest;
 use App\Http\Requests\Token\LoginRequest;
 use App\Http\Requests\Token\RegisterRequest;
 use App\Http\Resources\PersonalAccessTokenResource;
 use App\Models\PersonalAccessToken;
+use App\Models\Tenant;
 use App\Models\TenantUser;
 use App\Models\User;
 use App\Services\TokenService;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Namshi\JOSE\SimpleJWS;
 use Symfony\Component\HttpFoundation\Response;
@@ -22,6 +26,42 @@ class TokenController extends Controller
         private readonly User $user,
         private readonly TenantUser $tenantUser,
     ) {
+    }
+
+    public function index(TokenQuery $query, Tenant $tenant, Request $request): JsonResource
+    {
+        $this->authorize('viewAny', PersonalAccessToken::class);
+
+        $tenantUser = $this->tenantUser
+            ->where('user_id', auth()->user()->id)
+            ->where('tenant_id', $tenant->id)
+            ->first();
+
+        $tokens = $query
+            ->where('tenant_user_id', $tenantUser->id)
+            ->simplePaginate($request->get('limit', config('app.pagination_limit')))
+            ->appends($request->query());
+
+        return PersonalAccessTokenResource::collection($tokens);
+    }
+
+    public function store(CreateRequest $request, Tenant $tenant): JsonResource
+    {
+        $this->authorize('create', PersonalAccessToken::class);
+
+        $user = auth()->user();
+        $tenantUser = $this->tenantUser
+            ->where('user_id', $user->id)
+            ->where('tenant_id', $tenant->id)
+            ->first();
+
+        $input = $request->validated();
+
+        return new PersonalAccessTokenResource($this->authenticate(
+            $tenantUser,
+            $input['expiration_date'] ?? null,
+            $input['refresh'] ?? false,
+        ));
     }
 
     public function login(LoginRequest $request): JsonResource|JsonResponse
@@ -64,9 +104,12 @@ class TokenController extends Controller
         return response()->json(status: Response::HTTP_NO_CONTENT);
     }
 
-    private function authenticate(TenantUser $tenantUser): PersonalAccessToken
-    {
-        $service = new TokenService($tenantUser);
+    private function authenticate(
+        TenantUser $tenantUser,
+        ?int $expirationDate = null,
+        bool $refresh = false,
+    ): PersonalAccessToken {
+        $service = new TokenService($tenantUser, $expirationDate, $refresh);
         $token = $service->token();
         $payload = SimpleJWS::load($token)->getPayload();
 
